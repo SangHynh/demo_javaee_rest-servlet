70650065940125f98cdd4f6f3d62c85f77717af1
SangHynh <huynhvansang.020102@gmail.com>
2024-11-01 20:33:53 +0700

    add sql file

---
+++ account.sql
@@ -0,0 +1,11 @@
+INSERT INTO Account (username, email, address) VALUES
+('user1', 'user1@example.com', 'TPHCM'),
+('user2', 'user2@example.com', 'TPHCM'),
+('user3',  'user3@example.com', 'HN'),
+('user4', 'user4@example.com','TPHCM'),
+('user5',  'user5@example.com','HN'),
+('user6',  'user6@example.com', 'TPHCM'),
+('user7', 'user7@example.com','HN'),
+('user8', 'user8@example.com','HN'),
+('user9', 'user9@example.com','TPHCM'),
+('user10', 'user10@example.com','HN');
